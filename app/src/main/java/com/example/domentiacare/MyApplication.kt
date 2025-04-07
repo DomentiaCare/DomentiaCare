f2acc11f0bc189caab01f70c0d6ddb2cce4ca825
parkddddd <150421580+parkddddd@users.noreply.github.com>
2025-04-07 18:15:36 +0900

    토큰 받아오기 성공

--- app/src/main/java/com/example/domentiacare/MyApplication.kt
+++ app/src/main/java/com/example/domentiacare/MyApplication.kt
@@ -1,12 +1,39 @@
 package com.example.domentiacare
 
 import android.app.Application
+import android.content.Context
+import android.content.pm.PackageManager
+import android.util.Base64
+import android.util.Log
 import com.kakao.sdk.common.KakaoSdk
+import java.security.MessageDigest
 
 class MyApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         // Kakao SDK 초기화 (네이티브 앱 키 사용)
         KakaoSdk.init(this,  BuildConfig.KAKAO_NATIVE_APP_KEY)
+        getKeyHash(this)
+    }
+
+    // 키 해시 확인하는 함수. LogCat에 KeyHash에 출력됨. 이걸 카카오 디벨로퍼 콘솔에 등록해야 함.
+    fun getKeyHash(context: Context) {
+        try {
+            val info = context.packageManager.getPackageInfo(
+                context.packageName,
+                PackageManager.GET_SIGNING_CERTIFICATES
+            )
+            val signatures = info.signingInfo?.apkContentsSigners
+            if (signatures != null) {
+                for (signature in signatures) {
+                    val md = MessageDigest.getInstance("SHA")
+                    md.update(signature.toByteArray())
+                    val keyHash = Base64.encodeToString(md.digest(), Base64.NO_WRAP)
+                    Log.d("KeyHash", "Current Key Hash: $keyHash")
+                }
+            }
+        } catch (e: Exception) {
+            Log.e("KeyHash", "Unable to get key hash", e)
+        }
     }
 }
