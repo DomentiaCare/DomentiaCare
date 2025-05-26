2b7b496e5d053594e24fbc2bc49dd661c82e8cfc
parkddddd <150421580+parkddddd@users.noreply.github.com>
2025-05-26 14:12:02 +0900

    빌드 날리기

--- ChatApp/build/intermediates/cxx/Debug/4x1p494b/logs/arm64-v8a/build_command_chatapp.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Users\\hsooo\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "C:\\2025-1\\capstone\\project\\DomentiaCare\\ChatApp\\.cxx\\Debug\\4x1p494b\\arm64-v8a" ^
-  chatapp
